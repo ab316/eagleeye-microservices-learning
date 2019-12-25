b1e87dbd7336d4fc6e1a7510d7087276822702c1
Abdullah Baig <abdullahbaig456@gmail.com>
2019-12-25 20:30:34 +0100

    Add Zuul server

---
+++ zuul-server/src/main/java/com/learning/eagleeye/zuul/ZuulServerApplication.java
@@ -0,0 +1,17 @@
+package com.learning.eagleeye.zuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableZuulProxy
+@RefreshScope
+public class ZuulServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZuulServerApplication.class, args);
+	}
+
+}
